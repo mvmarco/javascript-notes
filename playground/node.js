5318d739e7807b7fb6da0d21fedc3359c83ea2ea
marco corapi <marco.corapi@outlook.it>
2021-01-06 18:20:23 +0100

    objects tricks

--- playground/node.js
+++
@@ -1,5 +0,0 @@
-/* to play with node, just call node on the terminal 
-- to call the results of console log you can do on the terminal:
-"node some.js" and the result comes */
-const marco = 300;
-console.log(marco);
