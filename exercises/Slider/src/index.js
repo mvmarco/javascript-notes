ea5291ae0e203441f1cdfdf3fc3abb3f3b432f89
Marco Corapi <marco.corapi@outlook.it>
2021-02-09 18:04:10 +0100

    more comments

--- exercises/Slider/src/index.js
+++ exercises/Slider/src/index.js
@@ -5,19 +5,55 @@
 
 // the slider refers to the div/elementwith the class of slider
 function Slider(slider) {
+  // here you say if the slider is not an element throw an error
+  /* 
+    instead of just passing if(!slider) {
+      throw new Error('No slider passed in');
+    }
+
+    in this case if you pass slider('jsjss') it would not be an error
+    because you pass a function with something in, no matter if it is not an element
+  */
   if (!(slider instanceof Element)) {
     throw new Error('No slider passed in');
   }
-  // create some variables for working iwth the slider
+  // create some variables for working with the slider
+  /* 
+    we need to keep track for prev, current and next to begin with
+    and they have to be empty because we will fill them up when the 
+    slider starts, and when the user navigates with previous and next
+    button.
+
+    prev, current and next are basically the SCSS classes associated to
+    the slide. We keep them here at the beginning
+  */
   let prev;
   let current;
   let next;
+
   // select the elements needed for the slider
   const slides = slider.querySelector('.slides');
   const prevButton = slider.querySelector('.goToPrev');
   const nextButton = slider.querySelector('.goToNext');
 
   function startSlider() {
+    /*
+      here we update/popilate the variables "prev", "current" and "next"
+      and we update within the function and not create a const inside
+      this startSlider with current, is because we have other functions
+      such as move(), applyClasses() and both of those functions
+      need to access the "current", "prev" and "next".
+
+      So if instead you create a const "prev" or "current" or "next"
+      that const would be accessible only inside this function. By creating
+      them inside the slider funtion all the other function inside can access
+      them.
+
+      That is the concept of "closures" having variables at higher level that 
+      can be accessed at the lower level. They are not global variables but
+      are variables that are living inside the closure slider function 
+    */
+
     current = slider.querySelector('.current') || slides.firstElementChild;
     prev = current.previousElementSibling || slides.lastElementChild;
     next = current.nextElementSibling || slides.firstElementChild;
