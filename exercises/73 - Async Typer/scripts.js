21a85555ec2237977918c776db72b277a6a37abb
Marco Corapi <marco.corapi@outlook.it>
2021-02-18 16:46:36 +0100

    letter async

--- exercises/73 - Async Typer/scripts.js
+++ exercises/73 - Async Typer/scripts.js
@@ -8,23 +8,32 @@ function wait(ms = 0) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+// this function will give a random number between a min and a max value
+// Math.random() generates a number from 0 to 1
+// getting a number from 10 and 15
+// min 20 millecond and max 150 - by doin gthis we get a number from 0 to 130 + 20 = 150
+// you store the value of MathRandom() in a variable so it keeps the same random number there and
+// does not keep changing
 function getRandomBetween(min = 20, max = 150, randomNumber = Math.random()) {
   return Math.floor(randomNumber * (max - min) + min);
 }
 
-// async for of loop
-// async function draw(el) {
-//   const text = el.textContent;
-//   let soFar = '';
-//   for (const letter of text) {
-//     soFar += letter;
-//     el.textContent = soFar;
-//     // wait for some amount of time
-//     const { typeMin, typeMax } = el.dataset;
-//     const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
-//     await wait(amountOfTimeToWait);
-//   }
-// }
+/* 
+  // async for of loop
+
+  async function draw(el) {
+    const text = el.textContent;
+    let soFar = '';
+    for (const letter of text) {
+      soFar += letter;
+      el.textContent = soFar;
+      // wait for some amount of time
+      const { typeMin, typeMax } = el.dataset;
+      const amountOfTimeToWait = getRandomBetween(typeMin, typeMax);
+      await wait(amountOfTimeToWait);
+    }
+  } 
+*/
 
 // recursion
 function draw(el) {
