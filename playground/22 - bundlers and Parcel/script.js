6e1353ae0295b7ee51a2e3658e0d3e622691bcd0
Marco Corapi <marco.corapi@outlook.it>
2021-02-28 17:23:26 +0100

    dev dependencies

--- playground/22 - bundlers and Parcel/script.js
+++ playground/22 - bundlers and Parcel/script.js
@@ -54,19 +54,38 @@
       },
       "author": "",
       "license": "ISC",
-      "devDependencies": {
-        "parcel-bundler": "^1.12.4"
-      },
-      "browserslist": [
-        "last 1 chrome versions"
-      ]
-    }
+     }
 
     3. After getting the "package.json" you can go ahead and install "parcel". You could either
        install it globally when you need a quick server up and running, but any project that is
        going on for more than a couple of days it is recommended to install "parcel" into that
        project folder as well. In that way, co-workers or who dowload the repo from GitHub will
-       have the same "parcel" version. 
+       have the same "parcel" version. And it is to let them get up and running with "npm start".
+       So for installing it we want to "npm install parcel-bundler" and you can save it as 
+       a "dev-dependency" by running: "npm install parcel-bundler --save-dev" what that would do
+       is to put it as dependency in our "package.json", under "dev-dependency" and this are
+       things not needed to let the application works/run but they are needed for someone to
+       work on the application itself. That is the difference between dependency, something like
+       "react" or "vue.js", and "dev-dependency" that is just a tooling needed to work on it. 
+       ******** check below 
+       
 
-
+       {
+         "name": "dadjokes",
+         "version": "1.0.0",
+         "description": "",
+         "main": "jokes.js",
+         "directories": {
+         "lib": "lib"
+         },
+         "scripts": {
+         "start": "parcel index.html",
+         "build": "parcel build index.html"
+         },
+         "author": "",
+         "license": "ISC",
+         "devDependencies": {
+         "parcel-bundler": "^1.12.4"  // ******** result of "npm install parcel-bundler --save-dev"
+         }
+        }
 */
