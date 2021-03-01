63050d89ead6e45ab446d497c71a01c56ce5d604
Marco Corapi <marco.corapi@outlook.it>
2021-03-01 12:07:15 +0100

    steps for parcel and package json

--- exercises/82 - npm modules/index.js
+++ exercises/82 - npm modules/index.js
@@ -9,8 +9,20 @@
       has dependencies itself, little packages that it needs in order for it to work. It is always
       fine to delete the "node_modules" folder because you can always get it back by installing
       "npm install". The thing is that we want some dependencies in our package.json. 
+  3. remove: "test": "echo \"Error: no test specified\" && exit 1" 
+     and add: "start": "parcel index.html" necessary to run parcel
+  4. in order to avoid any errors from the browser when we use async-await we could put:
+     "browserslist": ["last 1 chrome versions"]
+  5. run "npm start"
+
 */
 
 // #################################
 // INSTALLING OTHER DEPENDENCIES
 // #################################
+
+/* 
+  to install other depencies like "faker", "date-fns", "await-to-js", "lodash", "axios"
+  we can install them all together by putting a space between them as follow:
+  "npm i faker date-fns await-to-js lodash axios"
+*/
