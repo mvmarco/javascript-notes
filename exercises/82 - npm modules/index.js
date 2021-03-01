13427fc81b90a456d70dd178eb991734ac8d8fd3
Marco Corapi <marco.corapi@outlook.it>
2021-03-01 11:35:56 +0100

    installing other dependecies

--- exercises/82 - npm modules/index.js
+++ exercises/82 - npm modules/index.js
@@ -1,10 +1,16 @@
-// #######################
-// STEPS FOR PACKAGE JSON
-// #######################
+// #################################
+// STEPS FOR PACKAGE JSON AND PARCEL
+// #################################
 
 /* 
   1. "npm init" created the package.json directory
   2. "npm install parcel-bundler -D"  install parcel and put the web dependency (with shortcut)
       it creates also a "node_modules" folder with around 500 files. What happens is that parcel
-      has dependencies itself, little packages that it needs.
+      has dependencies itself, little packages that it needs in order for it to work. It is always
+      fine to delete the "node_modules" folder because you can always get it back by installing
+      "npm install". The thing is that we want some dependencies in our package.json. 
 */
+
+// #################################
+// INSTALLING OTHER DEPENDENCIES
+// #################################
