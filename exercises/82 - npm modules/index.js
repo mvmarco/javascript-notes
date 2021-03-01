391074713181d790b2dcd629fc5cf48b51aa4aed
Marco Corapi <marco.corapi@outlook.it>
2021-03-01 11:32:07 +0100

    npm init

---
+++ exercises/82 - npm modules/index.js
@@ -0,0 +1,10 @@
+// #######################
+// STEPS FOR PACKAGE JSON
+// #######################
+
+/* 
+  1. "npm init" created the package.json directory
+  2. "npm install parcel-bundler -D"  install parcel and put the web dependency (with shortcut)
+      it creates also a "node_modules" folder with around 500 files. What happens is that parcel
+      has dependencies itself, little packages that it needs.
+*/
