9faf5abd56991f9dda8ee657722ad56db36b9550
Marco Corapi <marco.corapi@outlook.it>
2021-03-01 13:21:39 +0100

    waait

--- exercises/82 - npm modules/index.js
+++ exercises/82 - npm modules/index.js
@@ -13,7 +13,7 @@
      and add: "start": "parcel index.html" necessary to run parcel
   4. in order to avoid any errors from the browser when we use async-await we could put:
      "browserslist": ["last 1 chrome versions"]
-  5. run "npm start"
+  5. run "npm start", in case of error cancel "cache" and "dist" and run again "npm start"
 
 */
 
@@ -24,5 +24,42 @@
 /* 
   to install other depencies like "faker", "date-fns", "await-to-js", "lodash", "axios"
   we can install them all together by putting a space between them as follow:
-  "npm i faker date-fns await-to-js lodash axios"
+  "npm i faker date-fns await-to-js lodash axios" and then "npm start"
 */
+
+// #################################
+// INSTALLING OTHER
+// #################################
+
+/* 
+  https://www.npmjs.com/package/waait this is the function we wrote all the time:
+
+  const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+
+  we could install it separetely with "npm i waait" and again "npm start"
+
+  in the doc the function and how to import it is:
+
+  import wait from 'waait';
+
+  async function doStuff() {
+    doSomething();
+    await wait();
+    doSomethingElse();
+    await wait(200);
+    console.log('200ms later');
+  }
+
+  so we can create a function as follow and import the "wait" with any name as it is a default
+  export. This is the smallest idea of what a package does, as small as a little function.
+*/
+
+import wait from 'waait';
+
+async function go() {
+  console.log('Going');
+  await wait(200);
+  console.log('ending');
+}
+
+go();
