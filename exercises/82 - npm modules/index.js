3a91063707eebea4b9292da6be7faef796dae8ac
mvmarco <marco.corapi@outlook.it>
2021-03-06 19:13:59 +0100

    test

--- exercises/82 - npm modules/index.js
+++ exercises/82 - npm modules/index.js
@@ -41,7 +41,7 @@
 // #################################
 
 /* 
-  1. "npm init" created the package.json directory
+  1. "npm init" created the package.json directory.
   2. "npm install parcel-bundler -D"  install parcel and put the web dependency (with shortcut)
       it creates also a "node_modules" folder with around 500 files. What happens is that parcel
       has dependencies itself, little packages that it needs in order for it to work. It is always
